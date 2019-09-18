666e47667d862c913908a2379b45c68747117dd2
Markus Sommer <msommer@1drop.de>
2019-09-18 22:01:24 +0200

    TASK: CGL

--- plugins/lazyload.js
+++ plugins/lazyload.js
@@ -1,7 +1,7 @@
-// @ts-ignore
 import LazyLoad from 'vanilla-lazyload';
 
 // eslint-disable-next-line no-new
 new LazyLoad({
+    // eslint-disable-next-line @typescript-eslint/camelcase
     elements_selector: '.lazy',
 });
