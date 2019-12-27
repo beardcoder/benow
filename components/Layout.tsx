58621216a14ec9d6286eba6b45b94ed923a572c5
Markus Sommer <msommer@1drop.de>
2019-12-27 21:07:05 +0100

    [TASK] Clean overlflow for page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,23 +10,35 @@ type Props = {
 
 const Layout: React.FunctionComponent<Props> = ({
     children,
-    title = 'This is the default title'
+    title = 'This is the default title',
 }) => (
-    <div>
-        <Head>
-            <title>{title}</title>
-            <meta charSet="utf-8" />
-            <meta
-                name="viewport"
-                content="initial-scale=1.0, width=device-width"
-            />
-            <link
-                href="https://fonts.googleapis.com/css?family=Maven+Pro:400,700|Roboto+Slab:400,700&display=swap"
-                rel="stylesheet"
-            />
-        </Head>
-        {children}
-    </div>
+    <>
+        <div className='app'>
+            <Head>
+                <title>{title}</title>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+                <link
+                    href='https://fonts.googleapis.com/css?family=Maven+Pro:400,700|Roboto+Slab:400,700&display=swap'
+                    rel='stylesheet'
+                />
+            </Head>
+            {children}
+        </div>
+        <style jsx>{`
+            .main {
+                position: relative;
+                z-index: 20;
+                overflow: hidden;
+                padding-bottom: 100px;
+            }
+            .app {
+                overflow: hidden;
+                position: relative;
+                z-index: 10;
+            }
+        `}</style>
+    </>
 );
 
 export default Layout;
