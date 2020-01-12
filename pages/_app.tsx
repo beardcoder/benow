4666eb59a97058291baa7047167b7710990226b7
Markus Sommer <msommer@1drop.de>
2020-01-12 08:02:12 +0100

    use composition api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { SocialProfileJsonLd } from 'next-seo';
 import App from 'next/app';
 import React from 'react';
 
@@ -7,6 +8,18 @@ export default class MyApp extends App {
         return (
             <>
                 <Component {...pageProps} />
+                <SocialProfileJsonLd
+                    type='Person'
+                    name='Markus Sommer'
+                    url='https://www.creativeworkspace.de'
+                    sameAs={[
+                        'https://github.com/beardcoder',
+                        'https://twitter.com/beardcoder',
+                        'https://forge.typo3.org/users/41461',
+                        'https://www.xing.com/profile/Markus_Sommer30',
+                        'https://www.linkedin.com/in/markus-sommer-9040649b/',
+                    ]}
+                />
             </>
         );
     }
