b4e6ebda58434e66201d01945bbde48b04bee24d
Markus Sommer <msommer@1drop.de>
2020-04-01 18:48:37 +0200

    Cleanup

--- src/helper/blogPostsXml.ts
+++ src/helper/blogPostsXml.ts
@@ -4,18 +4,17 @@ export default (blogPosts: IPost[]) => {
     let latestPost = '';
     let postsXml = '';
     Object.keys(blogPosts).map((key: any) => {
-        const postDate = blogPosts[key].data.date;
+        const postDate = blogPosts[key].date;
         if (
             !latestPost ||
-            new Date(blogPosts[key].data.date).getTime() >=
-                new Date(blogPosts[key].data.date).getTime()
+            new Date(blogPosts[key].date).getTime() >= new Date(blogPosts[key].date).getTime()
         ) {
             latestPost = postDate;
         }
         postsXml += `
             <url>
-                <loc>/blog/${blogPosts[key].data.slug}/</loc>
-                <lastmod>${blogPosts[key].data.date}</lastmod>
+                <loc>/blog/${blogPosts[key].slug}/</loc>
+                <lastmod>${blogPosts[key].date}</lastmod>
                 <priority>0.80</priority>
             </url>`;
     });
