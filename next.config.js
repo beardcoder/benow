e1374f98cc7a47dfcd3bbe71180fe9d00d2e55b5
Markus Sommer <msommer@1drop.de>
2020-04-07 22:01:39 +0200

    [TASK] Remove chunks

--- next.config.js
+++ next.config.js
@@ -26,38 +26,6 @@ module.exports = withPlugins(
                 test: /\.md$/,
                 use: 'raw-loader',
             });
-            config.plugins.push(
-                new webpack.optimize.LimitChunkCountPlugin({
-                    maxChunks: 5,
-                })
-            );
-            const splitChunks = config.optimization && config.optimization.splitChunks;
-            if (splitChunks) {
-                const cacheGroups = splitChunks.cacheGroups;
-                const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-                if (cacheGroups.framework) {
-                    cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-                        test: preactModules,
-                    });
-                    cacheGroups.commons.name = 'framework';
-                } else {
-                    cacheGroups.preact = {
-                        name: 'commons',
-                        chunks: 'all',
-                        test: preactModules,
-                    };
-                }
-            }
-
-            // inject Preact DevTools
-            if (dev && !isServer) {
-                const entry = config.entry;
-                config.entry = () =>
-                    entry().then(entries => {
-                        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
-                        return entries;
-                    });
-            }
             return config;
         },
         exportTrailingSlash: true,
