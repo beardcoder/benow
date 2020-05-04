c2beb775c9744252cb67433fe547ce70dce654c8
Markus Sommer <msommer@1drop.de>
2020-05-04 21:58:06 +0200

    switch back to react

--- next.config.js
+++ next.config.js
@@ -6,50 +6,10 @@ const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins([[optimizedImages]], {
     webpack(config, { isServer, dev }) {
-        if (isServer) {
-            config.externals = ['react', 'react-dom', ...config.externals];
-        }
-
-        const splitChunks = config.optimization && config.optimization.splitChunks;
-        if (splitChunks) {
-            const cacheGroups = splitChunks.cacheGroups;
-            const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-            if (cacheGroups.framework) {
-                cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-                    test: preactModules,
-                });
-                cacheGroups.commons.name = 'framework';
-            } else {
-                cacheGroups.preact = {
-                    name: 'commons',
-                    chunks: 'all',
-                    test: preactModules,
-                };
-            }
-        }
-
-        config.resolve.alias = Object.assign({}, config.resolve.alias, {
-            '~': path.join(__dirname),
-            react$: 'preact/compat',
-            'react-dom$': 'preact/compat',
-            react: 'preact/compat',
-            'react-dom': 'preact/compat',
-        });
-
         config.module.rules.push({
             test: /\.md$/,
             use: 'raw-loader',
         });
-
-        // inject Preact DevTools
-        if (dev && !isServer) {
-            const entry = config.entry;
-            config.entry = () =>
-                entry().then(entries => {
-                    entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
-                    return entries;
-                });
-        }
 
         return config;
     },
