e9aacc05c93d0c5fe2aa68a925be51600cd00e12
Markus Sommer <msommer@1drop.de>
2020-06-14 21:24:43 +0200

    ⚡️ (Reacrt): Use smalast image of the world

--- src/components/LazyImage.tsx
+++ src/components/LazyImage.tsx
@@ -4,7 +4,7 @@ import useLazyImg from 'react-use-lazy-img';
 export default function LazyImage({ src, ...props }: any) {
     const imgElement = useRef(null);
     const placeholderImage =
-        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII=';
+        'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
     const { imgSrc, onError } = useLazyImg(src, placeholderImage, imgElement);
     return <img src={imgSrc} ref={imgElement} {...props} onError={onError} />;
 }
