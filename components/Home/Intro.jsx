c00ce70d4939371056d8787e86509206d8c3de06
Markus Sommer <msommer@1drop.de>
2020-11-23 22:30:03 +0100

    feat: dark overlay on mobile

--- components/Home/Intro.jsx
+++ components/Home/Intro.jsx
@@ -4,14 +4,16 @@ import styles from './Intro.module.css'
 export default function HomeIntro() {
   return (
     <section id='intro' className={styles.intro}>
-      <Image
-        src='/content/images/header.jpg'
-        layout='fill'
-        alt='Header Image'
-        objectFit='cover'
-        className={styles.image}
-        quality={60}
-      ></Image>
+      <div className={styles.imageContainer}>
+        <Image
+          src='/content/images/header.jpg'
+          layout='fill'
+          alt='Header Image'
+          objectFit='cover'
+          className={styles.image}
+          quality={60}
+        ></Image>
+      </div>
       <div className='container z-10'>
         <div className={styles.text}>
           Innovation, Inspiration, Technik und Leidenschaft
