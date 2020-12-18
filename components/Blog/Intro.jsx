43c6cedb507a8e3fa0c635a191719425f7af2014
Markus Sommer <markussom@gmail.com>
2020-12-18 22:00:55 +0100

    fix

--- components/Blog/Intro.jsx
+++ components/Blog/Intro.jsx
@@ -1,5 +1,5 @@
 import styles from './Intro.module.css'
-import { Calendar, User, Tag } from 'react-icons/fi'
+import { FiCalendar, FiUser, FiTag } from 'react-icons/fi'
 import dayjs from 'dayjs'
 
 export default function BlogIntro({ title, image, createdAt, author, type }) {
@@ -13,14 +13,14 @@ export default function BlogIntro({ title, image, createdAt, author, type }) {
         <h1 className={styles.title}>{title}</h1>
         <div className='flex flex-row flex-wrap justify-center mt-16'>
           <div className='flex justify-center mb-4'>
-            <Calendar className='mr-3' />{' '}
+            <FiCalendar size="24" className='mr-3' />{' '}
             {dayjs(createdAt).locale('de-DE').format('DD.MM.YYYY')}
           </div>
           <div className='flex justify-center ml-10'>
-            <User className='mr-3' /> {author}
+            <FiUser size="24" className='mr-3' /> {author}
           </div>
           <div className='flex justify-center ml-10'>
-            <Tag className='mr-3' /> {type}
+            <FiTag size="24" className='mr-3' /> {type}
           </div>
         </div>
       </div>
