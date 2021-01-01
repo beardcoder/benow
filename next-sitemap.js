2619ef0cb755c271d6f07a9fa47c65ad96f4408b
Markus Sommer <Markus Sommer>
2021-01-01 21:57:25 +0100

    feat: add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+    siteUrl: 'https://creativeworkspace.de',
+    generateRobotsTxt: true, // (optional)
+  }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import '../styles/loader.css'
-import 'fontsource-poppins/700.css'
-import 'fontsource-ubuntu/400.css'
-import 'fontsource-ubuntu/700.css'
+import '@fontsource/poppins/700.css'
+import '@fontsource/ubuntu/400.css'
+import '@fontsource/ubuntu/700.css'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 
