3a6d248dfd73d12477bbc7730bcf857e35052b7e
Markus Sommer <markussom@gmail.com>
2021-11-22 22:40:21 +0100

    feat: use new config style

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net'],
     formats: ['image/avif', 'image/webp'],
   },
 }
+
+export default nextConfig
