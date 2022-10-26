416e8ba8439b1511ac61f114354eeb365f51e9eb
Markus Sommer <markussom@gmail.com>
2022-10-26 22:17:30 +0200

    feat: astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
 
-import image from "@astrojs/image";
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind({ config: { path: "./tailwind.config.cjs" } }),
-    image(),
+    tailwind({
+      config: {
+        path: './tailwind.config.cjs',
+      },
+    }),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
   ],
-});
+})
