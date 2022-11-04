2ce1a6b829fb214fd5a1d40a2b3fd68475204c03
Markus Sommer <markussom@gmail.com>
2022-11-04 22:44:55 +0100

    feat: rebuild structure

---
+++ src/components/snippet.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from 'react'
+import { GoLightBulb } from 'react-icons/go'
+
+import { ISnippet } from '@/@types/snippet'
+import { UiCard } from '@/src/components/card'
+
+type Props = ISnippet
+
+export default function Snippet({ url, description }: Props) {
+  return (
+    <UiCard href={`${url}`} target='_blank' rel='noreferrer'>
+      <div className='flex flex-row items-center'>
+        <div className='pt-1 pr-4'>
+          <GoLightBulb size='30' style={{ fill: 'url(#gradient)' }} />
+          <svg width='0' height='0'>
+            <linearGradient id='gradient' x1='100%' y1='100%' x2='0%' y2='0%'>
+              <stop stopColor='#84cc16' offset='0' />
+              <stop stopColor='#4ade80' offset='100%' />
+            </linearGradient>
+          </svg>
+        </div>
+        <h3 className='truncate'>{description}</h3>
+      </div>
+    </UiCard>
+  )
+}
