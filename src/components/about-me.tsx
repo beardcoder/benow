05e66462de101b4245e72b45dfa44fbd647436a4
Markus <9371658+beardcoder@users.noreply.github.com>
2023-02-06 08:39:11 +0000

    chore: change text

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -5,7 +5,11 @@ type Props = { image: string } & JSX.IntrinsicElements['section']
 export default function AboutMe({ ...props }) {
   const { image } = props
   return (
-    <section className='relative z-30 mx-auto md:flex max-w-screen-2xl' data-cy='personal' {...props}>
+    <section
+      className='relative z-30 mx-auto md:flex max-w-screen-2xl'
+      data-cy='personal'
+      {...props}
+    >
       <div className='w-full mr-20 md:w-1/2'>
         <Image
           data-cy='personalImage'
@@ -18,14 +22,21 @@ export default function AboutMe({ ...props }) {
       <div className='w-full px-5 my-auto mt-10 mb-20 md:mt-auto md:w-1/2 md:mb-auto'>
         <div className='relative z-10 flex flex-col mx-auto'>
           <h2 className='mb-5 text-4xl font-bold md:text-5xl'>
-            Hi, ich bin <span className='pb-1 text-gradient'>Markus Sommer&nbsp;</span>
+            Hi, ich bin{' '}
+            <span className='pb-1 text-gradient'>Markus Sommer&nbsp;</span>
           </h2>
           <p className='max-w-3xl text-lg prose dark:prose-invert text-opacity-70'>
-            Lass mich dir helfen, deine Online-Präsenz auf die nächste Stufe zu bringen! Meine Leidenschaft für die
-            Benutzererfahrung (User Experience) brennt lichterloh und ich bin bereit, all mein Wissen und meine
-            Erfahrung in dein Projekt zu investieren. Sei es durch eine Auffrischung deiner Website mit modernen
-            Web-Technologien, die Einrichtung einer Sitemap oder jeglichen anderen Rat, den du benötigst. Schreib mir
-            jetzt und lass uns gemeinsam dein Ziel erreichen!
+            Als UX-Profi bringe ich Fachwissen und Erfahrung in dein Projekt, um
+            deine Online-Präsenz zu optimieren. Meine Leidenschaft für die
+            Benutzererfahrung und mein tiefes Verständnis für moderne
+            Web-Technologien helfen dir, deine Website an die Bedürfnisse deiner
+            Zielgruppe anzupassen. Egal, ob du eine Auffrischung deiner Website
+            mit modernen Funktionen benötigst, eine benutzerfreundliche Sitemap
+            einrichten möchtest oder jeglichen anderen Rat brauchst, ich bin
+            bereit, dich zu unterstützen. Schreibe mir jetzt und lass uns
+            gemeinsam an deinem Erfolg arbeiten. Mit meiner Unterstützung kannst
+            du dein Ziel erreichen und deine Online-Präsenz auf ein neues Level
+            bringen.
           </p>
         </div>
       </div>
